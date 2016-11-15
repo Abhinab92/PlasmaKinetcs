960dc001aede2ca2c11af7e054d97f9baf485b2a
Abhinab92 <chodhry4@illinois.edu>
2016-11-14 19:10:34 -0600

    update

---
+++ Source_Code/Solver/RK4/rk4.h
@@ -0,0 +1,14 @@
+#ifndef RK4_H_
+#define RK4_H_
+
+class reaction{
+  private:
+    std::vector<double> sp1, sp2, sp3, sp4;
+    std::vector<double> k, k1, k2, k3, k4;
+    int iter;
+
+  public:
+    
+}
+
+#endif

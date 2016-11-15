89b705967a1572bf2399af39d6ef8584a9b3d505
Abhinab92 <chodhry4@illinois.edu>
2016-11-14 19:10:13 -0600

    update

---
+++ Source_Code/Solver/RK4/rk4.C
@@ -0,0 +1,16 @@
+#include"rk.h"
+
+reaction::solve_rxn(vector<double> sp, double end_time, double dt){
+  for (int i = 0; i < iter; i++){
+    sp1 = sp;
+    calc_change(k1,sp1,dt);
+    calc_val(sp2,sp1,k1,dt);
+    calc_change(k2,sp2,dt/double(2));
+    calc_val(sp3,sp2,k2,dt/double(2));
+    calc_change(k3,sp3,dt/double(2));
+    calc_val(sp4,sp3,k3,dt/double(2));
+    calc_change(k4,sp4,dt);
+    k = k1/double(6) + k2/double(3) + k3/double(3) + k4/double(6);
+    calc_val(sp,sp1,k,dt);
+  }
+}
